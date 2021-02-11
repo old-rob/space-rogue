dece7c1d68ac98ce7fecad788b6a779f20b90490
Robert Oldroyd <42311776+old-rob@users.noreply.github.com>
2021-02-11 14:18:40 -0700

    Update theGame.js

--- theGame.js
+++ theGame.js
@@ -15,6 +15,19 @@ class Actor {
   draw() {
     Game.display.draw(this.x, this.y, this.symbol, this.color);
   }
+  getPathTo(x, y) {
+    let passableCallback = function(x, y) {
+      return (x+","+y in Game.map);
+    }
+    let astar = new ROT.Path.AStar(x, y, passableCallback, {topology:4});
+
+    let path = [];
+    let pathCallback = function(x, y) {
+      path.push([x, y]);
+    }
+    astar.compute(this.x, this.y, pathCallback);
+    return path;
+  }
   act() {
     alert("Actor is acting");
     //return new Promise(resolve => alert("I'm an actor without acting instructions..."));
@@ -67,17 +80,28 @@ class Crab extends Actor {
   constructor(x, y) {
     super(x, y);
     this.symbol = "C";
+    this.speed = 5;
   }
   act() {
-    //alert("I'm a crab!");
-    this.draw();
+    let path = this.getPathTo(Game.player.x, Game.player.y);
+    path.shift(); /* remove position of actor */
+    if (path.length == 1) {
+      alert("Tag, you're it!");
+    } else {
+      let x = path[0][0];
+      let y = path[0][1];
+      Game.display.draw(this.x, this.y, Game.map[this.x+","+this.y]);
+      this.x = x;
+      this.y = y;
+      this.draw();
+    }
   }
 }
 
 Game = {
   display: null,
-  w: 25,
-  h: 25,
+  w: 35,
+  h: 35,
   player: null,
   actors: [],
   scheduler: null,
@@ -99,6 +123,18 @@ Game = {
 
     eventLoop();
   },
+
+  getCameraPos: function(playerPos, viewSize, mapSize) {
+    let halfView = viewSize / 2;
+    if (playerPos < halfView) {
+      return 0;
+    } else if (playerPos >= mapSize - halfView) {
+      return mapSize - viewSize;
+    } else {
+      return playerPos - halfView;
+    }
+  },
+
   drawMap: function(mapData) {
     for (let key in mapData) {
       let coord = key.split(",");
@@ -136,6 +172,7 @@ class MapBuilder {
     this.generateStars(10, freeCells);
     Game.player = this.createActor(Player, freeCells);
     Game.actors.push(this.createActor(Crab, freeCells));
+    Game.actors.push(this.createActor(Crab, freeCells));
 
     return this.map;
   }
@@ -161,7 +198,6 @@ class MapBuilder {
 async function eventLoop() {
     while (true) {
         let actor = Game.scheduler.next();
-        //alert(actor.symbol);
         if (!actor) { break; }
         await actor.act();
     }
@@ -170,6 +206,43 @@ async function eventLoop() {
 
 
 window.addEventListener("load", function() {
-  let builder = new MapBuilder(25, 25);
+  let builder = new MapBuilder(Game.w, Game.h);
   Game.initialize(builder.generateDugMap());
 });
+
+
+/*
+1D and 2D array conversion (width, index) {
+  index = x + width * y;
+  x = index % width;
+  y = index / width;
+}
+*/
+
+
+/*
+
+singalen commented on Mar 12, 2013
+Add onClick(x, y) and onHover(x, y) callbacks to level and/or display.
+Maybe even onSwipeProgress(x1, y1, x2, y2),
+onSwipeEnd(x1, y1, x2, y2) and so on for pinch for touch devices.
+
+ondras commented on Mar 15, 2013
+How exactly does this differ from addEventListener("click") or "mouseover" etc?
+
+singalen commented on Mar 16, 2013
+They would refer to a map coordinates instead of DOM element.
+Currently the map view is a single Canvas, if I get it right,
+and it I didn't see yet how to find the clicked/hovered map cell.
+
+ondras commented on Mar 16, 2013
+Okay, well it is probably sufficient to divide the mouse coord by
+the width/height of the map.
+But a helper function for this conversion (which would be far
+more complicated in hex layout) might be feasible. Accepting :-)
+
+ondras commented on Mar 17, 2013
+Implemented as ROT.Display::eventToPosition(e) -
+http://ondras.github.com/rot.js/doc/symbols/ROT.Display.html#eventToPosition
+
+*/
